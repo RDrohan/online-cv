a11f106bf9d7cb8533ee71daeb5d515b1293af33
Ricky Drohan <rdrohanirl@yahoo.ie>
2024-01-20 10:30:56 +0000

    Added light theme and changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,66 +2,42 @@ import React from "react";
 
 import "./Navbar.css";
 
-import { RiArrowDropDownLine } from "react-icons/ri";
-
 function Navbar() {
   return (
-    <nav className="w-full py-4 px-8 fixed top-0 z-50">
-      <ul className="max-w-5xl text-md sm:text-2xl mx-auto flex justify-between items-center">
-        <li>
-          <a href="#about" className="hover:text-red-600">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#skills" className="hover:text-red-600">
-            Skills
-          </a>
-        </li>
-        <li>
-          <a href="#education" className="hover:text-red-600">
-            Education
-          </a>
-        </li>
-        <li className="dropdown inline-block relative cursor-pointer">
-          <a className="py-2 px-4 inline-flex items-center hover:text-red-600">
-            Employment
-            <RiArrowDropDownLine className="hover:text-red-600" />
-          </a>
-          <ul className="dropdown-menu absolute bg-white hidden">
-            <li className="hover:text-red-600">
-              <a
-                className="rounded-t py-2 px-4 block whitespace-no-wrap"
-                href="#employment-1"
-              >
-                Page 1
-              </a>
-            </li>
-            <li className="hover:text-red-600">
-              <a
-                className="py-2 px-4 block whitespace-no-wrap"
-                href="#employment-2"
-              >
-                Page 2
-              </a>
-            </li>
-            <li className="hover:text-red-600">
-              <a
-                className="rounded-b py-2 px-4 block whitespace-no-wrap"
-                href="#employment-2"
-              >
-                Page 3
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#contact" className="hover:text-red-600">
-            Contact Me
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div className="navbar fixed justify-center bg-base-100">
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1 text-xs sm:text-lg">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <details>
+              <summary>Employment</summary>
+              <ul class="p-2 bg-base-100 w-full">
+                <li>
+                  <a href="#employment-1">Page 1</a>
+                </li>
+                <li>
+                  <a href="#employment-2">Page 2</a>
+                </li>
+                <li>
+                  <a href="#employment-2">Page 3</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a href="#contact">Contact Me</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
